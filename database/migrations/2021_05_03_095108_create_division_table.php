92a135aeedd2f2babf81e553bf5dcc3e5c28cca3
reymark <reymarkvaldehueza@gmail.com>
2021-05-03 10:54:19 +0800

    feature/92:Add division migration

---
+++ database/migrations/2021_05_03_095108_create_division_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDivisionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('division', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('division_code')->nullable();
+            $table->string('division_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('division');
+    }
+}
