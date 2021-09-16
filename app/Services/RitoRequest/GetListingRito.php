b6912bf768da4a6ad8900b3034a1aa7aa370b42e
Marwen Valeroso <marwenvalerosO@gmail.com>
2021-09-16 17:17:06 +0800

    Feature 199: RITO Request: Add Division & Section Column Field

--- app/Services/RitoRequest/GetListingRito.php
+++ app/Services/RitoRequest/GetListingRito.php
@@ -20,6 +20,9 @@ class GetListingRito
             'count' => $reqt->count
         ];
 
+        for ($i = 0; $i < count($results['data']); $i++) {
+            $results['data'][$i]->requestor = $this->requested_by($results['data'][$i]->requested_by)[0];
+        }
         return $results;
     }
 
@@ -45,9 +48,35 @@ class GetListingRito
             ),
         ));
 
-        $response = curl_exec($curl);
-
+        $response = json_decode(curl_exec($curl));
         curl_close($curl);
-        return json_decode($response);
+        return $response;
+    }
+
+    public function requested_by($emp_id)
+    {
+        $portal_token = System::where('handler', 'PORTAL_TOKEN')->pluck('value')->first();
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://caraga-portal.dswd.gov.ph/api/employee/list/search/?q=' . $emp_id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: ' . $portal_token,
+            ),
+        ));
+
+        $response = json_decode(curl_exec($curl));
+        curl_close($curl);
+        return $response;
     }
 }
